7562ede15adea30ee0169d5531e985910eedb31c
andriawanridwan <andriridwan787@gmail.com>
2020-04-07 07:33:51 +0700

    SIMAK Dharma Negara

---
+++ database/migrations/2020_04_06_234136_create_table_konsentrasi.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableKonsentrasi extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_konsentrasi', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('konsentrasi');
+            $table->string('prodi_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tb_konsentrasi');
+    }
+}
---
+++ database/migrations/2020_04_06_234430_create_table_tahun_akademik.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTableTahunAkademik extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tb_tahun_akademik', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('tahun_akademik');
+            $table->string('keterangan');
+            $table->date('batas_registrasi');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+public function down()
+    {
+        Schema::dropIfExists('tb_tahun_akademik');
+    }
+}
