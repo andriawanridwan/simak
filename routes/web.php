0594a33f3c8c915f8cb5e8eb9abf2720d59b3107
andriawanridwan <andriridwan787@gmail.com>
2020-04-09 10:11:53 +0700

    SIMAK Dharma Negara

--- routes/web.php
+++ routes/web.php
@@ -18,15 +18,3 @@
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
-Route::get('/ruangan','KelolaRuangan@index');
-Route::get('/dashboard','KelolaRuangan@dash');
-Route::post('/simpan_ruangan','KelolaRuangan@store');
-Route::get('/hapus_ruangan/{id}', 'KelolaRuangan@destroy');
-Route::get('/edit_ruangan/{id}', 'KelolaRuangan@edit');
-Route::post('/update_ruangan/{id}','KelolaRuangan@update');
-
-Route::get('/prodi','KelolaProdi@index');
-Route::post('/simpan_prodi','KelolaProdi@store');
-Route::get('/hapus_prodi/{id}', 'KelolaProdi@destroy');
-Route::get('/edit_prodi/{id}', 'KelolaProdi@edit');
-Route::post('/update_prodi/{id}','KelolaProdi@update');
